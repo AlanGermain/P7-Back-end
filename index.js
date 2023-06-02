853f8b41a0af340c7ff32715e08ff12050e6b71b
alan germain <alan.germain16@gmail.com>
2023-06-02 11:12:56 +0200

    p7 backend

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = 4000;
+
+app.use(cors());
+app.use(express.json());
+
+function sayHi(req, res) {
+  res.send("Hello World");
+}
+
+app.get("/", sayHi);
+app.post("/api/auth/signup", signUp);
+app.post("/api/auth/login", login);
+
+app.listen(PORT, function () {
+  console.log(`server is running on : ${PORT}`);
+});
+
+const users = [];
+
+function signUp(req, res) {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const userInDb = users.find((user) => user.email === email);
+  console.log("userInDb:", userInDb);
+  if (userInDb != null) {
+    res.status(400).send("Email Already exists");
+    return;
+  }
+  const user = {
+    email: email,
+    password: password,
+  };
+  users.push(user);
+  res.send("sign up");
+}
+
+function login(req, res) {
+  const body = req.body;
+  console.log("body:", body);
+
+  const userInDb = users.find((user) => user.email === body.email);
+  if (userInDb == null) {
+    res.status(401).send("wrong credentials");
+    return;
+  }
+  const passwordInDb = userInDb.password;
+  if (passwordInDb != body.password) {
+    res.status(401).send("wrong credentials");
+    return;
+  }
+
+  res.send({
+    userId: "123",
+    token: "token",
+  });
+}
