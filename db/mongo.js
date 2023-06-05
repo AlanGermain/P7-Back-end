82317b1433cba01f708af009b668ae8954930d4c
alan germain <alan.germain16@gmail.com>
2023-06-05 15:00:24 +0200

    hash

--- db/mongo.js
+++ db/mongo.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-const DB_URL = `mongodb+srv://${USER}:${PASSWORD}@${DB_DOMAIN}`;
+const DB_URL = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@${process.env.DB_DOMAIN}`;
 console.log("DB_URL:", DB_URL);
 async function connect() {
   try {
